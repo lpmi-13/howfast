69ac4e89cdac0624743034ed6b43ec16a2e95b50
Adam Leskis <leskis@gmail.com>
2020-04-08 08:02:58 +0100

    update from names to time

--- src/components/TimePicker.js
+++ src/components/TimePicker.js
@@ -1,22 +1,22 @@
 import React, {Component} from 'react';
 import Picker from 'react-mobile-picker-scroll';
 
+// make an array of the numbers 0 - 59 for the minutes and seconds
+const zeroToFiftyNineRange = [...Array(60).keys()].map(t => t.toString());
+
 class TimePicker extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+  state = {
       valueGroups: {
-        title: 'Mr.',
-        firstName: 'Micheal',
-        secondName: 'Jordan'
+        hours: '0',
+        minutes: '0',
+        seconds: '0'
       }, 
       optionGroups: {
-        title: ['Mr.', 'Mrs.', 'Ms.', 'Dr.'],
-        firstName: ['John', 'Micheal', 'Elizabeth'],
-        secondName: ['Lennon', 'Jackson', 'Jordan', 'Legend', 'Taylor']
+        hours: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+        minutes: zeroToFiftyNineRange,
+        seconds: zeroToFiftyNineRange
       }
     };
-  }
 
   // Update the value in response to user picking event
   handleChange = (name, value) => {
